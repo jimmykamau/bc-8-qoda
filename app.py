e1fe553bfbb3d219e110476b85faa2a9931908b9
Jimmy Kamau <jimmykambiz@gmail.com>
2016-06-23 17:20:24 +0300

    add functionality to invite users to session

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from flask_mail import Mail, Message
 from forms import newUserForm, editUserForm, changeUserPass, \
     StringField, validators, PasswordField
 
+# Initialize app and app's variables
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
@@ -21,13 +22,13 @@ mail = Mail(app)
 sockets = Sockets(app)
 redis = redis.from_url(app.config['REDIS_URL'])
 
-
+# Import other modules. Not imported at top of file due to cyclic imports error 
 from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required, current_user, utils
+    UserMixin, RoleMixin, login_required, current_user
 from code import *
 from chat import *
 
-# Define models
+# Define flask-security models
 roles_users = db.Table('roles_users',
                         db.Column('user_id', db.Integer(),
                                     db.ForeignKey('user.id')),
@@ -56,10 +57,20 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'email' : self.email,
+            'full_name' : self.full_name,
+        }
+
+
     def __repr__(self):
         return (self.email)
 
 
+# Import RegisterForm from flask-security for Login's customization
 from flask_security.forms import RegisterForm
 
 
@@ -72,12 +83,15 @@ security = Security(app, user_datastore,
                     confirm_register_form=ExtendedRegisterForm)
 
 
-from models import CodeSessions
+# Import models
+from models import CodeSessions, CurrentUserSession, CodingSessionUsers
 
 
 admin = Admin(app, name='Qoda', template_mode='bootstrap3')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(CodeSessions, db.session))
+#admin.add_view(ModelView(CurrentUserSession, db.session))
+admin.add_view(ModelView(CodingSessionUsers, db.session))
 
 
 def create_new_coding_session(session_name):
@@ -88,18 +102,65 @@ def create_new_coding_session(session_name):
     return new_session.id
 
 
+def change_user_current_sesion(session_id):
+    try:
+        current_user_session = db.session.query(CurrentUserSession).filter_by(user_id=int(current_user.id)).one()
+    except:
+        current_user_session = CurrentUserSession()
+
+    current_user_session.user_id = int(current_user.id)
+    current_user_session.session_id = int(session_id)
+    db.session.add(current_user_session)
+    db.session.commit()
+    return current_user_session.session_id
+
+
+def add_user_to_coding_session(s_id, u_id):
+    new_user = db.session.query(CodingSessionUsers).filter_by(user_id=u_id,
+                                                            session_id=s_id).all()
+    if new_user:
+        pass
+    else:
+        new_user = CodingSessionUsers(user_id=u_id, session_id=s_id)
+        db.session.add(new_user)
+        db.session.commit()
+    return new_user
+
+
+def check_user_owns_session(session_id):
+    try:
+        sess = db.session.query(CodeSessions).filter_by(id=session_id).one()
+        if sess.session_owner == str(current_user.email):
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def get_registered_users():
+    return db.session.query(User).filter_by(active=True).all()
+
+
 @app.route('/')
 @login_required
 def index():
-    return render_template('index.html', user=str(current_user.full_name))
+    try:
+        session_request = db.session.query(CurrentUserSession).filter_by(user_id=int(current_user.id)).one()
+        current_session = session_request.session_id
+    except:
+        current_session = 1
+    return render_template('index.html', user=str(current_user.full_name), \
+                        chan=current_session, \
+                        user_list=[u.serialize for u in get_registered_users()])
 
 
 @app.route('/create_new_session')
 def create_new_session():
     session_name = request.args.get('name', type=str)
     new_session = create_new_coding_session(session_name)
-    session['current_session'] = new_session
-    return jsonify(session_id=session['current_session'])
+    change_user_current_sesion(new_session)
+    return jsonify(session_id=new_session)
 
 
 @app.route('/my_sessions')
@@ -110,9 +171,27 @@ def get_user_sessions():
 
 @app.route('/sessions/<int:session_number>')
 def change_session(session_number):
-    session['current_session'] = session_number
-    return render_template('index.html', user=str(current_user.full_name))
+    current_session = change_user_current_sesion(session_number)
+    return render_template('index.html', 
+                            user=str(current_user.full_name), chan=current_session, 
+                            user_list=[u.serialize for u in get_registered_users()])
 
+
+@app.route('/add_user_to_session/<session_id>/<user_id>')
+def add_user(session_id, user_id):
+    if check_user_owns_session(int(session_id)):
+        add_user_to_coding_session(int(session_id), int(user_id))
+        return jsonify(message="Success")
+    else:
+        return jsonify(message="Fail")
+
+
+@app.route('/get_session_users/<session_id>')
+def get_session_users(session_id):
+    session_id = int(session_id)
+    user_dets = db.session.query(CodingSessionUsers).filter_by(
+        session_id=session_id).all()
+    return jsonify(users=[u.serialize for u in user_dets])
 
 if __name__ == '__main__':
     app.run()
--- static/js/application.js
+++ static/js/application.js
@@ -2,9 +2,10 @@ if (window.location.protocol == "https:") {
 	var ws_scheme = "wss://";
 } else {
 	var ws_scheme = "ws://"
-};
+}; 
 
-var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive_code");
-var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit_code");
+
+var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
+var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
 var inbox_chat = new ReconnectingWebSocket(ws_scheme + location.host + "/receive_chat");
 var outbox_chat = new ReconnectingWebSocket(ws_scheme + location.host + "/submit_chat");
