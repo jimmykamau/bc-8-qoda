2d6466f029cdacfbafd012c355300361aa0d52ac
Jimmy Kamau <jimmykambiz@gmail.com>
2016-06-24 02:40:21 +0300

    add styling

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import redis
 import gevent
 from flask import Flask, render_template, \
-    url_for, redirect, session, request, json, \
-    jsonify
+    url_for, redirect, request, \
+    jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CsrfProtect
 from flask_sockets import Sockets
@@ -94,9 +94,10 @@ admin.add_view(ModelView(CodeSessions, db.session))
 admin.add_view(ModelView(CodingSessionUsers, db.session))
 
 
-def create_new_coding_session(session_name):
+def create_new_coding_session(session_name, session_desc=''):
     new_session = CodeSessions(session_owner=str(current_user),
-                                session_name=session_name)
+                            session_name=session_name,
+                            session_desc=session_desc)
     db.session.add(new_session)
     db.session.commit()
     return new_session.id
@@ -112,7 +113,8 @@ def change_user_current_sesion(session_id):
     current_user_session.session_id = int(session_id)
     db.session.add(current_user_session)
     db.session.commit()
-    return current_user_session.session_id
+    current_session_name = db.session.query(CodeSessions).filter_by(id=int(current_user_session.session_id)).one()
+    return current_user_session.session_id, current_session_name.session_name, current_session_name.session_desc
 
 
 def add_user_to_coding_session(s_id, u_id):
@@ -149,16 +151,18 @@ def index():
         session_request = db.session.query(CurrentUserSession).filter_by(user_id=int(current_user.id)).one()
         current_session = session_request.session_id
     except:
-        current_session = 1
-    return render_template('index.html', user=str(current_user.full_name), \
-                        chan=current_session, \
-                        user_list=[u.serialize for u in get_registered_users()])
+        current_u_session = CodeSessions(session_owner=str(current_user.email), session_name="My first session", session_desc="My first Qoda coding session")
+        db.session.add(current_u_session)
+        db.session.commit()
+        current_session = current_u_session.id
+    return redirect(url_for('change_session', session_number=current_session))
 
 
 @app.route('/create_new_session')
 def create_new_session():
     session_name = request.args.get('name', type=str)
-    new_session = create_new_coding_session(session_name)
+    session_description = request.args.get('desc')
+    new_session = create_new_coding_session(session_name, session_description)
     change_user_current_sesion(new_session)
     return jsonify(session_id=new_session)
 
@@ -171,9 +175,10 @@ def get_user_sessions():
 
 @app.route('/sessions/<int:session_number>')
 def change_session(session_number):
-    current_session = change_user_current_sesion(session_number)
+    new_session_id, new_session_name, new_session_desc = change_user_current_sesion(session_number)
     return render_template('index.html', 
-                            user=str(current_user.full_name), chan=current_session, 
+                            user=current_user, chan=new_session_id, 
+                            chan_name=new_session_name, chan_desc=new_session_desc, 
                             user_list=[u.serialize for u in get_registered_users()])
 
 
@@ -192,6 +197,48 @@ def get_session_users(session_id):
     user_dets = db.session.query(CodingSessionUsers).filter_by(
         session_id=session_id).all()
     return jsonify(users=[u.serialize for u in user_dets])
+
+
+@app.route('/edit_session')
+def edit_session():
+    session_id = request.args.get('session_id')
+    session_name = request.args.get('session_name')
+    session_desc = request.args.get('session_desc')
+    if check_user_owns_session(int(session_id)):
+        current_session = db.session.query(CodeSessions).filter_by(id=int(session_id)).one()
+        current_session.session_name = session_name
+        current_session.session_desc = session_desc
+        db.session.add(current_session)
+        db.session.commit()
+        return jsonify(message="Pass")
+    else:
+        return jsonify(message="Fail")
+
+
+@app.route("/delete_session")
+def delete_session():
+    session_id = request.args.get('session_id')
+    if check_user_owns_session(int(session_id)):
+        sesssion_to_delete = db.session.query(CodeSessions).filter_by(id=int(session_id)).one()
+        delete_coding_session_userstore = db.session.query(CodingSessionUsers).filter_by(session_id=int(session_id)).all()
+        unset_current_session = db.session.query(CurrentUserSession).filter_by(user_id=int(current_user.id)).one()
+        db.session.delete(unset_current_session)
+        for i in delete_coding_session_userstore:
+            db.session.delete(i)
+        db.session.delete(sesssion_to_delete)
+        db.session.commit()
+        try:
+            cuurrent_session = db.session.query(CodeSessions).filter_by(session_owner=str(current_user.email)).one()
+            current_session = cuurrent_session.id
+        except:
+            current_u_session = CodeSessions(session_owner=str(current_user.email), session_name="My first session", session_desc="My first Qoda coding session")
+            db.session.add(current_u_session)
+            db.session.commit()
+            current_session = current_u_session.id
+        flash("Session deleted successfully", 'success')
+        return redirect(url_for('change_session', session_number=current_session))
+    else:
+        return jsonify(message="Unauthorized")
 
 if __name__ == '__main__':
     app.run()
